36bf400b4b6d3f5931f05f160986ce6f6ab3e5e4
rogerdpack <rogerpack2005@gmail.com>
2011-05-28 11:25:05 -0600

    ice ;)

--- comparison/go.rb
+++ comparison/go.rb
@@ -1,9 +1,12 @@
+# right answer:
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229
+
 def calcFibN(num)
      fib_array = [0,1]
      (num-2).times do |i|
          fib_array << (fib_array[i] + fib_array[i+1])
      end
-     #puts fib_array.join(", ")
+     #fib_array.join(", ")
 end
 30_00_000.times {
   calcFibN(30)
