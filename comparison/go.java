e45ea229937ed512e682c41caeed2c938ee24d01
rogerdpack <rogerpack2005@gmail.com>
2011-05-30 10:23:17 -0600

    more comparisonseez

---
+++ comparison/go.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class go {
+
+
+  static void calcFibN(int num) {
+     ArrayList<Integer> fib_array = new ArrayList<Integer>(Arrays.asList(new Integer(0), new Integer(1)));
+    for(int i = 0; i < num - 2; i++) {
+      //fib_array << (fib_array[i] + fib_array[i+1])
+      fib_array.add(fib_array.get(i) + fib_array.get(i+1));
+     }
+    //System.out.println(fib_array);
+  }
+
+  public static void main(String[] args) {
+    for(int i = 0; i < 3000000; i++) {
+      calcFibN(30);
+    }
+  }
+
+}
---
+++ comparison/run_jruby_server.bat
@@ -0,0 +1,1 @@
+j --server go.rb
