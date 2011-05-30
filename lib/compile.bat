c74f5ffd870c111a925a91495dec74f34723d9be
rogerdpack <rogerpack2005@gmail.com>
2011-05-30 11:24:28 -0600

    compile

--- lib/compile.bat
+++ lib/compile.bat
@@ -1,3 +1,2 @@
-call j -S mirahc std_lib_stevia  profile_stev specs_stev
-call j -S mirahc profile_stev 
-call j -S mirahc specs_stev
+@rem needs to come first :)
+call j -S mirahc std_lib_stevia && call j -S mirahc profile_stev && call j -S mirahc specs_stev
